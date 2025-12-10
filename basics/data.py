901d97f6f2ae75a557f705f0f3ffbeada608767d
BvoJa <giabao13122006@gmail.com>
2025-12-10 23:23:13 +0700

    update

--- basics/data.py
+++ basics/data.py
@@ -6,7 +6,7 @@ from datasets import load_dataset
 from transformers import AutoTokenizer 
 
 class DataModule(pl.LightningDataModule):
-    def __init__(self, model_name="google/bert_uncased_L-2_H-128_A-2", batch_size=32, max_length=512):
+    def __init__(self, model_name="google/bert_uncased_L-2_H-128_A-2", batch_size=64, max_length=512):
         super().__init__()
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
